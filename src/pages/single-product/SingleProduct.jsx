bc638459cd0e2ad2f50f70f4457091e30605705f
Satyam Kumar <satyamsingh748846@gmail.com>
2024-01-21 20:57:52 +0530

    loader in single page

--- src/pages/single-product/SingleProduct.jsx
+++ src/pages/single-product/SingleProduct.jsx
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import toast from "react-hot-toast";
 import { useParams } from "react-router-dom";
-import { FaFilter } from "react-icons/fa";
+import ClipLoader from "react-spinners/ClipLoader";
 import Card from "../../components/card/Card";
 import { useAppContext } from "../../context/Context";
 const SingleProduct = () => {
@@ -15,12 +15,15 @@ const SingleProduct = () => {
 	useEffect(() => {
 		const getSingleProductData = async () => {
 			try {
+				setLoading(true);
 				const response = await axios.get(
 					`https://fakestoreapi.com/products/${id}`
 				);
+				setLoading(false);
 				setSingleProduct(response.data);
 				console.log("single product: ", response.data);
 			} catch (error) {
+				setLoading(false);
 				console.log("Error while fetching data from server", error);
 				toast.error("Something went wrong");
 			}
@@ -30,12 +33,20 @@ const SingleProduct = () => {
 	return (
 		<div className="single-product-page">
 			<div className="single-product">
-				{singleProduct && (
-					<div className="left">
-						<div className="image-container">
-							<img src={singleProduct.image} alt="" />
-						</div>
+				{loading ? (
+					<div className="single-loader">
+						<ClipLoader
+							color="#3e5a94"
+							loading={loading}
+							size={50}
+							aria-label="Loading Spinner"
+							data-testid="loader"
+						/>
 					</div>
+				) : (
+					<div className="new-single-product">
+
+          </div>
 				)}
 			</div>
 			<div className="related-products">
