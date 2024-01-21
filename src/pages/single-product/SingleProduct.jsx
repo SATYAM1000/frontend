2f3ffdc62875d3c9835671bbbb03746a66af18f3
Satyam Kumar <satyamsingh748846@gmail.com>
2024-01-21 22:20:18 +0530

    images added

--- src/pages/single-product/SingleProduct.jsx
+++ src/pages/single-product/SingleProduct.jsx
@@ -8,6 +8,7 @@ import { useParams } from "react-router-dom";
 import ClipLoader from "react-spinners/ClipLoader";
 import Card from "../../components/card/Card";
 import { useAppContext } from "../../context/Context";
+import placeholder from "../../assets/placeholder.jpg";
 const SingleProduct = () => {
 	const [singleProduct, setSingleProduct] = useState({});
 	const { loading, setLoading, allProducts } = useAppContext();
@@ -45,8 +46,34 @@ const SingleProduct = () => {
 					</div>
 				) : (
 					<div className="new-single-product">
-
-          </div>
+						<div className="top-div">
+							<div className="image-box">
+								<img src={singleProduct.image} alt={singleProduct.title} />
+							</div>
+							<div className="text-box">
+								<h3 className="pp-title">{singleProduct.title}</h3>
+								<p className="pp-description">{singleProduct.description}</p>
+								<h4>{singleProduct.price}</h4>
+								<div className="quantity-add-cart">
+									<div className="qty"></div>
+									<div className="cart-btn">
+										<button>Add to cart</button>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div className="bottom-div">
+							<div className="img1">
+								<img src={placeholder} alt="" />
+							</div>
+							<div className="img2">
+								<img src={placeholder} alt="" />
+							</div>
+							<div className="img3">
+								<img src={placeholder} alt="" />
+							</div>
+						</div>
+					</div>
 				)}
 			</div>
 			<div className="related-products">
