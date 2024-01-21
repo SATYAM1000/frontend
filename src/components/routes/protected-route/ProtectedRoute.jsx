7de9c545277345c768a4ad651393c014668f7600
Satyam Kumar <satyamsingh748846@gmail.com>
2024-01-21 19:10:17 +0530

    routes

--- src/components/routes/protected-route/ProtectedRoute.jsx
+++ src/components/routes/protected-route/ProtectedRoute.jsx
@@ -1,10 +1,12 @@
 import { Navigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 const ProtectedRoute = (props) => {
   if(localStorage.getItem("token")){
     return props.children;
   }
   else{
+    toast.error("Please login first");
     return <Navigate to="/login" />
   }
 }
